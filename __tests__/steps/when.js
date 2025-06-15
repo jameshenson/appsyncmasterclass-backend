086ce6ce2791bad97d84abb06225248e2c4be084
James Henson <james.c.henson@gmail.com>
2025-06-15 12:46:33 -0400

    added Jest and created our first e2e test and integration

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const AWS = require('aws-sdk')
+
 const we_invoke_confirmUserSignup = async (username, name, email) =>{
     const handler = require('../../functions/confirm-user-signup').handler
     const context = {}
@@ -25,7 +27,40 @@ const we_invoke_confirmUserSignup = async (username, name, email) =>{
 
 }
 
+const a_user_signs_up = async (password, name, email) => {
+    const cognito = new AWS.CognitoIdentityServiceProvider()
+    const userPoolId = process.env.COGNITO_USER_POOL_ID
+    const clientId = process.env.WEB_COGNITO_USER_POOL_CLIENT_ID
+
+    const signUpResp = await cognito.signUp({
+        ClientId: clientId,
+        Username: email,
+        Password: password,
+        UserAttributes: [
+            {Name: 'name', Value: name}
+        ]
+    }).promise()
+
+    const username = signUpResp.UserSub
+    console.log(`[${email}] - user has signed up [${username}]`)
+
+    //This function allows us to skip the confirmation code (ususally delivered by email) and verify users in cognito
+    await cognito.adminConfirmSignUp({
+        UserPoolId: userPoolId,
+        Username: username,
+    }).promise()
+
+    console.log(`[${email}] - email confirmed sign up`)
+
+    return{
+        username,
+        name,
+        email
+    }
+
+}
 
 module.exports = {
-    we_invoke_confirmUserSignup
+    we_invoke_confirmUserSignup, 
+    a_user_signs_up
 }
--- functions/confirm-user-signup.js
+++ functions/confirm-user-signup.js
@@ -1,30 +1,35 @@
-const DynamoDB = require('aws-sdk/clients/dynamodb')
-const DocumentClient = new DynamoDB.DocumentClient()
-const Chance = require('chance')
-const chance = new Chance()
-const {USERS_TABLE} = process.env 
-module.exports.handler = async (event) =>{
-    if(event.triggerSource === 'PostConfirmation_ConfirmSignUp'){
-        const name = event.request.userAttributes['name']
-        const suffix = chance.string({ lenght:8, casing: 'upper', alpha: true, numeric: true})
-        const screenName = `${name.replace(/[^a-zA-Z0-9]/g,"")}${suffix}`
-        const user = {
-            id: event.userName,
-            name,
-            screenName,
-            createdAt: new Date().toJSON(),
-            followersCount: 0,
-            followingCount: 0,
-            tweetsCount: 0,
-            likesCounts: 0,
-       }
-        await DocumentClient.put({
-                TableName: USERS_TABLE,
-                Item: user, 
-                ConditionExpression: 'attribute_not_exists(id)'
-            }).promise()
-        return event
-    }else{
-        return event
-    }
-}
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const Chance = require("chance");
+const chance = new Chance();
+
+const USERS_TABLE = process.env.USERS_TABLE;
+//this should work
+exports.handler = async (event) => {
+  if (event.triggerSource === "PostConfirmation_ConfirmSignUp") {
+    const name = event.request.userAttributes["name"];
+    const suffix = chance.string({ length: 8, casing: "upper", alpha: true, numeric: true });
+    const screenName = `${name.replace(/[^a-zA-Z0-9]/g, "")}${suffix}`;
+ 
+    const user = {
+      id: { S: event.userName },
+      name: { S: name },
+      screenName: { S: screenName },
+      createdAt: { S: new Date().toJSON() },
+      followersCount: { N: "0" },
+      followingCount: { N: "0" },
+      tweetsCount: { N: "0" },
+      likesCounts: { N: "0" },
+    };
+
+    const client = new DynamoDBClient();
+    const command = new PutItemCommand({
+      TableName: USERS_TABLE,
+      Item: user,
+      ConditionExpression: "attribute_not_exists(id)",
+    });
+
+    await client.send(command);
+  }
+
+  return event;
+};
